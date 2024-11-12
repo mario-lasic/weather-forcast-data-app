b66218de02e7c842f7e6c0711935e3465dd35831
Mario Lasić <mario.lasic@outlook.com>
2024-11-12 17:11:49 +0100

    Add Sky, Change to metric units, Add Try block

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ from api_key import API_key
 import requests
 
 def get_data(place, forecast_days=None):
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}&units=metric"
     response = requests.get(url=url)
     data = response.json()
     filtered_data = data["list"]
--- main.py
+++ main.py
@@ -11,17 +11,31 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-#Get data
-    filtered_data = get_data(place, days)
+    #Get data
+    try:
+        filtered_data = get_data(place, days)
+        if option == "Temperature":
+        # Create a temperature plot
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature(C)"})
+            st.plotly_chart(figure)
 
-    if option == "Temperature":
-    # Create a temperature plot
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature(C)"})
-        st.plotly_chart(figure)
+        if option == "Sky":
+            sky = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            image_path = [images[condition] for condition in sky]
+            # Number of columns
+            num_cols = 8
 
-    if option == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-        st.image()
+            # Iterate over the images and display them in rows with 8 columns each
+            for i in range(0, len(image_path), num_cols):
+                cols = st.columns(num_cols)
+                for col, img in zip(cols, image_path[i:i+num_cols]):
+                    col.image(img, use_container_width=True)
+    except KeyError:
+        st.text(f"{place} does not exists.")
+
+    
+
 
